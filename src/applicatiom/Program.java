dd3528be8ad4074d6647e3f9a671856e5bdd0fbb
EvilAlves <wagner.ap@outlook.com>
2021-07-14 14:32:23 -0300

    Starting to implement Board and Piece

---
+++ src/applicatiom/Program.java
@@ -0,0 +1,15 @@
+package applicatiom;
+
+import boardgame.Position;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Position pos = new Position(3, 5);
+		
+		
+		System.out.println(pos);
+	}
+
+}
