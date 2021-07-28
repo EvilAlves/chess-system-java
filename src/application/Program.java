8723a3cff7331caba7822c95b70b02b56266ccb8
EvilAlves <wagner.ap@outlook.com>
2021-07-28 12:03:40 -0300

    ChessException and ChessPosition

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,10 +4,8 @@ import chess.ChessMatch;
 
 
 public class Program {
-	
 	public static void main(String[] args) {
 		
-
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
 		
