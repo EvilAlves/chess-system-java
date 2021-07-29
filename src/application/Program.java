befb213f5a15c4886ab57dd38748e5ecc4c71640
EvilAlves <wagner.ap@outlook.com>
2021-07-29 13:02:17 -0300

    Exception move and possile move Rook

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,47 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import chess.ChessException;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 
 public class Program {
 	public static void main(String[] args) {
+		/*
+		 * Conteudo original do curso de https://github.com/acenelio
+		 * Material aprendido pelo curso https://www.udemy.com/course/java-curso-completo/
+		 * 
+		 */
+		Scanner sc = new Scanner(System.in);
 		
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while(true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch(ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+		}
 		
 	}
 
